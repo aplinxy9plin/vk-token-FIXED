a4b02d71f6f63ea1a36708837a6936ad17b5c763
Nikita Aplin <aplinxy9plin@icloud.com>
2018-04-05 12:41:26 +0700

    Fucking working

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 var vktoken = require('vk-token-FIXED')
-vktoken.getAccessToken('nikis@sibnet.ru', 'poshelnaxooypidor', function(error, token){
+const readline = require('readline');
 
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+vktoken.getAccessToken('', '', function(error, token){
+  rl.question('Enter code: ', (code) => {
+    vktoken.twoStep(token, code, function(){
+      console.log(token);
+      console.log(code);
+    })
+  })
 })
+//vktoken.test()
